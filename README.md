6c0b8fc118c5719c9f6d3070680f9d657b671ade
MaggieLOL <tanhanze@qq.com>
2024-03-29 22:24:48 +0800

    docs: add more infos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OVCore
 Core of open-vocaloid.
+
+Developed in Python 3.12.0.
---
+++ ovcore/__init__.py
@@ -0,0 +1,7 @@
+"""
+OVCore - OpenVocaloid Core Library
+"""
+
+__version__ = "0.1.0"
+__author__ = "OpenVocaloid Project"
+__license__ = "MIT"
